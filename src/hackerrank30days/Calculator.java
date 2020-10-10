e581b45937b8351acd7e8dab590312e4597e3769
dencilappavoo <appavoo@gmail.com>
2020-10-10 13:18:26 +0800

    Day17Exceptions

---
+++ src/hackerrank30days/Calculator.java
@@ -0,0 +1,17 @@
+package hackerrank30days;
+
+class Calculator
+{
+    public static void main(String[] args) {}
+    int power(int n,int p) throws Exception
+    {
+        if(n<0 || p<0){
+            throw new Exception("n and p should be non-negative");
+        }
+        else
+            {
+            return (int)Math.pow(n,p);
+        }
+    }
+}
+
