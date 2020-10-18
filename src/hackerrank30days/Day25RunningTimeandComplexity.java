f7b6cafc02371887d7afac7525edd8e024720e22
dencilappavoo <appavoo@gmail.com>
2020-10-19 05:59:47 +0800

    Day23BSTLevelOrder

--- src/hackerrank30days/Day25RunningTimeandComplexity.java
+++ src/hackerrank30days/Day25RunningTimeandComplexity.java
@@ -105,7 +105,7 @@ public class Day25RunningTimeandComplexity {
 
     static boolean isPrime(int n) {
         if(n==2 || n==3) return true;
-        if (n==0 || n==1 || n%2==0) return false;
+        if (n == 1 || n % 2 == 0) return false;
         for(int i=3;i*i<=n;i+=2) {
             if(n%i==0)
                 return false;
