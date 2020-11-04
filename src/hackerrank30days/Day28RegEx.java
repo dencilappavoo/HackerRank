33dd1542a3751f6b5cb44a4f9e89e4e89b6ef346
dencilappavoo <appavoo@gmail.com>
2020-11-05 00:54:48 +0800

    commit

--- src/hackerrank30days/Day28RegEx.java
+++ src/hackerrank30days/Day28RegEx.java
@@ -9,7 +9,7 @@ public class Day28RegEx {
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        LinkedList<String> gmailNames = new LinkedList<>();
+        LinkedList<String> gmailNames = new LinkedList<String>();
         int arrayCount = 0;
 
         for (int NItr = 0; NItr < N; NItr++) {
