5ee96ae37972a1b488f57904583f7f2d5658227a
dencilappavoo <appavoo@gmail.com>
2020-11-04 15:28:23 +0800

    Day28RegEx

--- src/hackerrank30days/Day28RegEx.java
+++ src/hackerrank30days/Day28RegEx.java
@@ -10,7 +10,6 @@ public class Day28RegEx {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
         LinkedList<String> gmailNames = new LinkedList<>();
-        String[] arrayNames = new String[N];
         int arrayCount = 0;
 
         for (int NItr = 0; NItr < N; NItr++) {
@@ -21,8 +20,6 @@ public class Day28RegEx {
             if(emailID.endsWith("@gmail.com"))
             {
                 gmailNames.add(firstName);
-                Array.set(arrayNames,arrayCount,firstName);
-                ++arrayCount;
             }
         }
         scanner.close();
